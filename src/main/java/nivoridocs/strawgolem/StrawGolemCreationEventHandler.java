b599c88b4e64e7503bad55fd471fb550e8ab5062
Brian Dixon <brianide@gmail.com>
2020-04-15 03:38:38 -0500

    Automatic block placement can now complete a golem
    Previously a golem would only be spawned if the second block was placed
    by a player. This prevent automatic block placement (eg. by a Botania
    flower) from completing golems.

--- src/main/java/nivoridocs/strawgolem/StrawGolemCreationEventHandler.java
+++ src/main/java/nivoridocs/strawgolem/StrawGolemCreationEventHandler.java
@@ -1,42 +1,46 @@
 package nivoridocs.strawgolem;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import nivoridocs.strawgolem.entity.EntityStrawGolem;
 
 @EventBusSubscriber
 public class StrawGolemCreationEventHandler {
-	
+
 	@SubscribeEvent
-	public static void onBlockPlaceEvent(BlockEvent.PlaceEvent event) {
-		World worldIn = event.getWorld();
-		BlockPos pos = event.getPos();
-		Block block = event.getState().getBlock();
-		
-		BlockPos pumpkin;
-		BlockPos hay;
-		
-		if (block == Blocks.PUMPKIN) {
-			pumpkin = pos;
-			hay = pos.down();
-		} else if (block == Blocks.HAY_BLOCK) {
-			pumpkin = pos.up();
-			hay = pos;
-		} else return;
-		
-		if (checkStructure(worldIn, hay, pumpkin)) {
-			pos = hay;
-			worldIn.setBlockState(pumpkin, Blocks.AIR.getDefaultState());
-			worldIn.setBlockState(hay, Blocks.AIR.getDefaultState());
-			EntityStrawGolem strawGolem = new EntityStrawGolem(worldIn);
-			strawGolem.setPosition(getCoord(pos.getX()), pos.getY(), getCoord(pos.getZ()));
-			worldIn.spawnEntity(strawGolem);
-		}
+	public static void onWorldLoad(WorldEvent.Load event) {
+		if(!event.getWorld().isRemote)
+			event.getWorld().addEventListener(new WorldEventListenerAdapter() {
+				@Override
+				public void notifyBlockUpdate(World worldIn, BlockPos pos, IBlockState oldState, IBlockState newState, int flags) {
+					Block block = newState.getBlock();
+					BlockPos pumpkin, hay;
+
+					if(block == Blocks.PUMPKIN) {
+						pumpkin = pos;
+						hay = pos.down();
+					} else if (block == Blocks.HAY_BLOCK) {
+						pumpkin = pos.up();
+						hay = pos;
+					} else return;
+
+					if (checkStructure(worldIn, hay, pumpkin)) {
+						pos = hay;
+						worldIn.setBlockState(pumpkin, Blocks.AIR.getDefaultState());
+						worldIn.setBlockState(hay, Blocks.AIR.getDefaultState());
+						EntityStrawGolem strawGolem = new EntityStrawGolem(worldIn);
+						strawGolem.setPosition(getCoord(pos.getX()), pos.getY(), getCoord(pos.getZ()));
+						worldIn.spawnEntity(strawGolem);
+					}
+				}
+			});
 	}
 	
 	private static double getCoord(int c) {
